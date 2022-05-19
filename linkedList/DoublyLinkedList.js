2d2577dbd949434b5c9c12fe0f5351864ac34ea7
elsid01 <ely_mbaye@yahoo.com>
2022-05-19 01:10:13 -0700

    doublyLinkedlist implemented

---
+++ linkedList/DoublyLinkedList.js
@@ -0,0 +1,12 @@
+
+
+class DoublyNode extends Node{
+   constructor(element, next, prev){
+     super(element, next);
+     this.prev = prev;
+   }
+}
+
+class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals)
+}
